3de3c203bcc606192e2871dcf7f7d12f1f6db9f8
niemand8080 <benhamster443@gmail.com>
2025-05-10 12:03:34 +0200

    Added some hotkeys to hammerspoon

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,5 +1,9 @@
 hs = hs
 
+-- 
+-- App launcher
+--
+
 hs.hotkey.bind({ "cmd" }, "1", function()
     hs.application.launchOrFocus("Ghostty")
 end)
@@ -16,8 +20,36 @@ hs.hotkey.bind({ "cmd" }, "4", function()
     hs.application.launchOrFocus("Finder")
 end)
 
-hs.hotkey.bind({ "option" }, "A", function()
-    local window = hs.window.focusedWindow()
-    window:maximize(0)
+--
+-- Windows
+--
+
+local margin = 8
+local almost_maximize_percentage = 0.9
+
+hs.hotkey.bind({ "option" }, "f", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local max = win:screen():frame()
+
+    f.x = max.x + margin
+    f.y = max.y + margin
+    f.w = max.w - margin * 2
+    f.h = max.h - margin * 2
+    win:setFrame(f, 0)
 end)
 
+hs.hotkey.bind({ "option" }, "a", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local max = win:screen():frame()
+
+    local w = max.w * almost_maximize_percentage
+    local h = max.h * almost_maximize_percentage
+
+    f.x = max.x + (max.w - w) / 2
+    f.y = max.y + (max.h - h) / 2
+    f.w = w
+    f.h = h
+    win:setFrame(f, 0)
+end)
