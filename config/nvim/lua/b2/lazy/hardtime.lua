565783105daee84b0ddf0729b9eadb40b32f9061
niemand8080 <benhamster443@gmail.com>
2025-07-04 16:35:45 +0200

    dont use gg, use go

--- config/nvim/lua/b2/lazy/hardtime.lua
+++ config/nvim/lua/b2/lazy/hardtime.lua
@@ -1,5 +1,15 @@
 return {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {}
+    opts = {
+        disabled_keys = {
+            ["gg"] = { "n" },
+        },
+        ["gg"] = {
+            message = function()
+               return "Use go instead of gg"
+            end,
+            length = 2,
+        },
+    }
 }
