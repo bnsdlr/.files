cf82a93f0474ca5301f9acc6e93d4fcb6e29cf8f
bnsdlr <benhamster443@gmail.com>
2025-07-25 19:46:47 +0200

    update hardtimes nvim plugin opts

--- config/nvim/lua/plugins/hardtime.lua
+++ config/nvim/lua/plugins/hardtime.lua
@@ -1,15 +1,4 @@
 return {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {
-        disabled_keys = {
-            ["gg"] = { "n" },
-        },
-        ["gg"] = {
-            message = function()
-               return "Use go instead of gg"
-            end,
-            length = 2,
-        },
-    }
 }
