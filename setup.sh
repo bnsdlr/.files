4b49314a2fc7380600867d526672c4bfb0f3d42c
bnsdlr <benhamster443@gmail.com>
2025-08-18 19:51:38 +0200

    disable macos Ctrl + Space keybind

--- setup.sh
+++ setup.sh
@@ -22,6 +22,8 @@ defaults write com.apple.dock mru-spaces -bool true
 defaults write com.apple.finder AppleShowAllExtensions -bool true
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 defaults write com.apple.finder FXRemoveOldTrashItems -bool true
+# disable Ctrl + Space, because of tmux prefix
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 60 "{ enabled = 0; value = { parameters = (32, 49, 262144); type = standard; }; }"
 # Change the hammerspoon config path
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 
