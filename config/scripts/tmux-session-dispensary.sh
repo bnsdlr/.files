14b36c9edd23c75b8b1bfe870e6c0cd856d70ffb
bnsdlr <benhamster443@gmail.com>
2025-08-23 16:53:46 +0200

    update tmux-session-dispensary.sh to check if fd and sk are installed

--- config/scripts/tmux-session-dispensary.sh
+++ config/scripts/tmux-session-dispensary.sh
@@ -7,9 +7,22 @@ DIRS=(
     "$(pwd)"
 )
 
+check_if_installed() {
+    cli_tool=$1
+    git_url=$2
+    if ! which $cli_tool; then
+        printf "Make sure $cli_tool is installed ($git_url)\n"
+        echo "Press enter to exit"
+        read
+        exit 1
+    fi
+}
+
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
+    check_if_installed fd "https://github.com/sharkdp/fd"
+    check_if_installed sk "https://github.com/skim-rs/skim"
     selected=$(fd . "${DIRS[@]}" --type=dir --max-depth=2 --full-path \
         | sed -E "s|^$HOME/(.*)/$|\1|" \
         | sk --margin 10% --color="bw")
@@ -18,11 +31,16 @@ fi
 
 [[ ! $selected ]] && exit 0
 
-selected_name=$(basename "$selected" | tr . _)
+selected_name=$(basename "$selected" | tr '. ' _)
+
 if ! tmux has-session -t "$selected_name"; then
     tmux new-session -ds "$selected_name" -c "$selected"
     tmux select-window -t "$selected_name:1"
 fi
 
-tmux switch-client -t "$selected_name"
+if [[ "$TMUX" == "" ]]; then
+    tmux attach -t "$selected_name"
+else
+    tmux switch-client -t "$selected_name"
+fi
 
