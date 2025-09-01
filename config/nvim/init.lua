57e30eb3b56d3c0001f2ddd61ab13cff371a4b36
bnsdlr <benhamster443@gmail.com>
2025-09-01 09:43:19 +0200

    update allacrity, ghostty, and nvim config to use vague theme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,112 +1,57 @@
-local function contains(t, value)
-    for _, v in pairs(t) do
-        if v == value then
-            return true
-        end
-    end
-    return false
-end
-
-vim.api.nvim_create_autocmd('FileType', {
-    desc = 'File type specific settings',
-    callback = function(filetype)
-        local ext = filetype.match
-
-        if contains({ 'markdown', 'elixir' }, ext) then
-            vim.opt.tabstop = 2
-            vim.opt.softtabstop = 2
-            vim.opt.shiftwidth = 2
-            vim.opt.expandtab = true
-        else
-            vim.opt.tabstop = 4
-            vim.opt.softtabstop = 4
-            vim.opt.shiftwidth = 4
-            vim.opt.expandtab = true
-        end
-    end,
-})
-
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.winborder = "single"
-
-vim.opt.smartindent = true
-
 vim.opt.swapfile = false
--- vim.opt.directory = os.getenv("HOME") .. "/.vim/tmp/swap//" -- Custom swap file location
-
-vim.opt.backup = true
-vim.opt.backupdir = os.getenv("HOME") .. "/.vim/tmp/backup//"
-
 vim.opt.undofile = true
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
 vim.opt.wrap = false
-
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
-
 vim.opt.colorcolumn = "79"
-
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = "number"
-
 vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
 vim.opt.signcolumn = "yes"
 
--- Remaps
+local map = vim.keymap.set
 vim.g.mapleader = " "
 
-local map = vim.keymap.set
-
-map("n", "<leader>pv", vim.cmd.Ex)
+map("i", "<C-L>", "<C-O><C-L>")
 
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
-
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
-
 map("n", "n", "nzz")
 map("n", "N", "Nzz")
 
 map({ "n", "v", "x" }, "<leader>y", "\"+y")
-map({ "n", "v", "x" }, "<leader>Y", "\"+y$")
-
-map({ 'n', 'v', 'x' }, '<leader>d', '\'_d')
-map({ 'n', 'v', 'x' }, '<leader>D', '\'_d$')
-
-map({ "n", "v", "x" }, "<leader>S", "\"_cc")
+map({ 'n', 'v', 'x' }, '<leader>d', '\"_d')
+map({ "n", "v", "x" }, "<leader>c", "\"_c")
 
 map('n', '<leader>s', ':e #<CR>')
 
 map({ 'n', 'v' }, '<leader>cw', '1z=')
-
+map("n", "<leader>pv", vim.cmd.Ex)
 map('n', '<leader>cn', ':cnext<CR>')
 map('n', '<leader>cp', ':cprev<CR>')
 map('n', '<leader>cc', ':cclose<CR>')
 map('n', '<leader>co', ':copen<CR>')
-map('n', '<leader>cf', [[:vimgrep /\<<C-r><C-w>\>/ **/*<CR>]])
 
 -- plugins
 vim.pack.add({
-    { src = "https://github.com/rose-pine/neovim" },
-    { src = "https://github.com/echasnovski/mini.pick" },
-    { src = "https://github.com/mbbill/undotree" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-    -- lsp
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = 'https://github.com/Saghen/blink.cmp' },
-    -- language plugins
-    { src = "https://github.com/mrcjkb/rustaceanvim" },
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/mbbill/undotree" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	-- lsp
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = 'https://github.com/Saghen/blink.cmp' },
+	-- language plugins
+	{ src = "https://github.com/mrcjkb/rustaceanvim" },
 })
 
 require "run"
@@ -123,5 +68,5 @@ map('n', '<leader>pg', ':Pick grep tool=\'rg\'<CR> <CR>')
 require("nvim-treesitter.configs").setup({ highlight = { enable = true } })
 
 -- colorscheme
-vim.cmd('colorscheme rose-pine')
+vim.cmd('colorscheme vague')
 vim.cmd('hi statusline guibg=NONE')
