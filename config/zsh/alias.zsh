6f85df31d2edb4239252247a22439d8332c2f631
bnsdlr <benhamster443@gmail.com>
2025-09-22 20:34:31 +0200

    fixed aliases

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -1,4 +1,4 @@
-pg() {
+g() {
     p=$(sk --margin 10% --regex --ansi -i -c 'rg --color=always --line-number "{}"')
 
     if (( $? == 0 )); then
@@ -7,7 +7,7 @@ pg() {
 
         p="${p%%:*}"
 
-        nvim "$file" +$num
+        nvim "$p" +$num
     fi
 }
 
@@ -24,7 +24,7 @@ alias doc="cd $HOME/documents"
 alias p="cd $HOME/documents/projects"
 alias edu="cd $HOME/documents/projects/edu"
 alias conf="cd $HOME/.config"
-alias dot="cd $HOME/.dotfiles"
+alias dot="cd $DOTFILES"
 
 if command -v exercism &>/dev/null; then
 	alias exer="cd $(exercism workspace)"
