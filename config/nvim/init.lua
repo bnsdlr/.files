40fb04e975bbc29cdf9aa420ee4db68714324047
bnsdlr <benhamster443@gmail.com>
2025-10-06 20:08:36 +0200

    update nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,7 @@
+vim.cmd([[set mouse=]])
+
+vim.opt.winborder = "single"
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
@@ -14,13 +18,136 @@ vim.opt.scrolloff = 8
 vim.opt.ignorecase = true
 vim.opt.signcolumn = "yes"
 
+-- plugins
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/aznhe21/actions-preview.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim",         version = "0.1.8" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", 		version = "main" },
+	{ src = "https://github.com/mbbill/undotree" },
+	-- lsp
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = 'https://github.com/Saghen/blink.cmp' },
+	-- language plugins
+	{ src = "https://github.com/mrcjkb/rustaceanvim" },
+})
+
+require "telescope".setup({
+	defaults = {
+		preview = false,
+		color_devicons = true,
+		sorting_strategy = "ascending",
+		borderchars = { "", "", "", "", "", "", "", "" },
+		path_displays = "smart",
+		layout_strategy = "horizontal",
+		layout_config = {
+			height = 100,
+			width = 400,
+			prompt_position = "top",
+			preview_cutoff = 0,
+		}
+	}
+})
+require("actions-preview").setup {
+	backend = { "telescope" },
+	extensions = { "env" },
+	telescope = vim.tbl_extend(
+		"force",
+		require("telescope.themes").get_dropdown(), {}
+	)
+}
+
+require("nvim-treesitter.configs").setup({ highlight = { enable = true } })
+
+require("oil").setup({
+	lsp_file_methods = {
+		enabled = true,
+		timeout_ms = 1000,
+		autosave_changes = true,
+	},
+	columns = {
+		"permissions",
+		"icon",
+	},
+	float = {
+		max_width = 0.7,
+		max_height = 0.6,
+		border = "rounded",
+	},
+})
+
+require("oil").setup({
+	lsp_file_methods = {
+		enabled = true,
+		timeout_ms = 1000,
+		autosave_changes = true,
+	},
+	columns = {
+		"permissions",
+		"icon",
+	},
+	float = {
+		max_width = 0.7,
+		max_height = 0.6,
+		border = "rounded",
+	},
+})
+
+local function pack_clean()
+	local active_plugins = {}
+	local unused_plugins = {}
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		active_plugins[plugin.spec.name] = plugin.active
+	end
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		if not active_plugins[plugin.spec.name] then
+			table.insert(unused_plugins, plugin.spec.name)
+		end
+	end
+
+	if #unused_plugins == 0 then
+		print("No unused plugins.")
+		return
+	end
+
+	local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
+	if choice == 1 then
+		vim.pack.del(unused_plugins)
+	end
+end
+
+vim.keymap.set("n", "<leader>pc", pack_clean)
+
+local builtin = require("telescope.builtin")
 local map = vim.keymap.set
 vim.g.mapleader = " "
 
+map({ "n" }, "<leader>f", builtin.find_files, { desc = "Telescope live grep" })
+map({ "n" }, "<leader>g", builtin.live_grep, { desc = "Telescope live grep" })
+map({ "n" }, "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
+map({ "n" }, "<leader>si", builtin.grep_string, { desc = "Telescope live string" })
+map({ "n" }, "<leader>so", builtin.oldfiles, { desc = "Telescope buffers" })
+map({ "n" }, "<leader>sh", builtin.help_tags, { desc = "Telescope help tags" })
+map({ "n" }, "<leader>sm", builtin.man_pages, { desc = "Telescope man pages" })
+map({ "n" }, "<leader>sr", builtin.lsp_references, { desc = "Telescope tags" })
+map({ "n" }, "<leader>st", builtin.builtin, { desc = "Telescope tags" })
+map({ "n" }, "<leader>sd", builtin.registers, { desc = "Telescope tags" })
+map({ "n" }, "<leader>sc", builtin.colorscheme, { desc = "Telescope tags" })
+map({ "n" }, "<leader>sa", require("actions-preview").code_actions)
+
+map("n", "<leader>u", vim.cmd.UndotreeToggle)
+
 map("i", "<C-L>", "<C-O><C-L>")
 
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", "@=:m '>+1<CR>gv=gv")
+map("v", "K", "@=:m '<-2<CR>gv=gv")
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 map("n", "n", "nzz")
@@ -33,7 +160,7 @@ map({ "n", "v", "x" }, "<leader>c", "\"_c")
 map('n', '<leader>s', ':e #<CR>')
 
 map({ 'n', 'v' }, '<leader>cw', '1z=')
-map("n", "<leader>e", vim.cmd.Ex)
+map("n", "<leader>e", ':Oil<CR>')
 
 map('n', '<leader>cn', ':cnext<CR>')
 map('n', '<leader>cp', ':cprev<CR>')
@@ -43,32 +170,47 @@ map('n', '<leader>co', ':copen<CR>')
 map('n', '<leader>n', ':next<CR>')
 map('n', '<leader>p', ':prev<CR>')
 
--- plugins
-vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/mbbill/undotree" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-	-- lsp
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = 'https://github.com/Saghen/blink.cmp' },
-	-- language plugins
-	{ src = "https://github.com/mrcjkb/rustaceanvim" },
+-- lsp
+require "mason".setup()
+
+vim.lsp.enable({
+	"lua_ls",
+	"jsonls",
 })
 
-require "run"
-require "lsp"
+-- vim.api.nvim_create_autocmd('LspAttach', {
+-- 	group = vim.api.nvim_create_augroup('my.lsp', {}),
+-- 	callback = function(args)
+-- 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+-- 		if client:supports_method('textDocument/completion') then
+-- 			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+-- 			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+-- 			client.server_capabilities.completionProvider.triggerCharacters = chars
+-- 			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+-- 		end
+-- 	end,
+-- })
+-- vim.cmd [[set completeopt+=menuone,noselect,popup]]
 
-map("n", "<leader>u", vim.cmd.UndotreeToggle)
+require('blink.cmp').setup({
+	fuzzy = { implementation = "lua" },
+	sources = { default = { 'lsp', 'path', 'buffer' } },
+	completion = {
+		documentation = { auto_show = true, auto_show_delay_ms = 0 },
+		menu = { max_height = 10 }
+	},
+	keymap = {
+		preset = 'default',
+		['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+		['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+	}
+})
 
-require "mini.pick".setup()
-map('n', '<leader>f', ':Pick files tool=\'rg\'<CR>')
-map('n', '<leader>h', ':Pick help tool=\'rg\'<CR>')
-map('n', '<leader>g', ':Pick grep tool=\'rg\'<CR><CR>')
-map('n', '<leader>lg', ':Pick grep_live tool=\'rg\'<CR>')
-
-require("nvim-treesitter.configs").setup({ highlight = { enable = true } })
+map('n', 'K', function() vim.lsp.buf.hover { max_height = 25, max_width = 120 } end)
+map('n', 'gd', vim.lsp.buf.definition)
+map('n', 'gt', vim.lsp.buf.type_definition)
+map('n', '<leader>ld', vim.diagnostic.open_float)
+map('n', '<leader>lf', vim.lsp.buf.format)
 
 -- colorscheme
 vim.cmd('colorscheme vague')
