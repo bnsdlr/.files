e4fbd494fb09c1215f7c7880d070b2d405b334ec
bnsdlr <me@bsdlr.de>
2026-03-08 16:34:16 +0100

    ...

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -4,7 +4,9 @@ c.window.hide_decoration = True
 c.completion.shrink = True
 
 config.bind('go', ':scroll-to-perc 0')
-config.source('vague.py')
+config.bind('<space>h', ':home')
+config.bind('<space>p', ':tab-pin')
+config.bind('<space>s', ':config-source')
 
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.policy.page = 'always'
@@ -12,8 +14,9 @@ c.colors.webpage.preferred_color_scheme = 'dark'
 
 css = "~/.config/qutebrowser/my.css"
 config.set("content.user_stylesheets", [css])
-config.bind(",n", f'config-cycle content.user_stylesheets ["{css}"] []')
+config.bind("<space>n", f'config-cycle content.user_stylesheets ["{css}"] []')
 
 c.url.default_page = "about:blank"
 c.url.start_pages = ["about:blank"]
 
+config.source('vague.py')
