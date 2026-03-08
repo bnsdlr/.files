6f263bf3ab9f88f496246829fbc04735408f849b
bnsdlr <me@bsdlr.de>
2026-03-08 20:13:37 +0100

    updated zsh

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -3,10 +3,23 @@ config.load_autoconfig(False)
 c.window.hide_decoration = True
 c.completion.shrink = True
 
+c.editor.command = [
+    "alacritty",
+    "--class",
+    "com.alacritty.floatingedit",
+    "-e",
+    "zsh",
+    "-c",
+    "export DONT_START_TMUX=1; $HOME/.local/share/bob/nvim-bin/nvim {file}"
+]
+
 config.bind('go', ':scroll-to-perc 0')
 config.bind('<space>h', ':home')
 config.bind('<space>p', ':tab-pin')
 config.bind('<space>s', ':config-source')
+config.bind('<space>o', ':open -t about:blank')
+config.bind('<Ctrl-n>', ':tab-next')
+config.bind('<Ctrl-p>', ':tab-prev')
 
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.policy.page = 'always'
