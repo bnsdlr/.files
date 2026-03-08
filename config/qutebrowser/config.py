920462e9441b9fda60470c314ed87c96e3648145
bnsdlr <me@bsdlr.de>
2026-03-08 20:51:19 +0100

    updated qutebrowser config

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -10,7 +10,7 @@ c.editor.command = [
     "-e",
     "zsh",
     "-c",
-    "export DONT_START_TMUX=1; $HOME/.local/share/bob/nvim-bin/nvim {file}"
+    "export DONT_START_TMUX=1; $HOME/.local/share/bob/nvim-bin/nvim {file} \"+call cursor({line}, {column})\""
 ]
 
 config.bind('go', ':scroll-to-perc 0')
@@ -20,6 +20,16 @@ config.bind('<space>s', ':config-source')
 config.bind('<space>o', ':open -t about:blank')
 config.bind('<Ctrl-n>', ':tab-next')
 config.bind('<Ctrl-p>', ':tab-prev')
+config.bind('<Ctrl-u>', ':edit-url')
+config.bind('<Ctrl-e>', ':edit-text', mode='insert')
+config.bind('<Ctrl-e>', ':cmd-edit')
+config.bind('<Ctrl-1>', ':tab-focus 1')
+config.bind('<Ctrl-2>', ':tab-focus 2')
+config.bind('<Ctrl-3>', ':tab-focus 3')
+config.bind('<Ctrl-4>', ':tab-focus 4')
+config.bind('<Ctrl-5>', ':tab-focus 5')
+config.bind('<Ctrl-6>', ':tab-focus 6')
+config.bind('<Ctrl-7>', ':tab-focus 7')
 
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.policy.page = 'always'
