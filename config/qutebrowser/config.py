8336ecb68654bf371665e79fec90141622c99bc8
bnsdlr <me@bsdlr.de>
2026-03-11 17:23:47 +0100

    ...

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -42,7 +42,7 @@ c.colors.webpage.preferred_color_scheme = 'dark'
 
 css = "~/.config/qutebrowser/my.css"
 config.set("content.user_stylesheets", [css])
-config.bind("<space>n", f'config-cycle content.user_stylesheets ["{css}"] []')
+config.bind(",n", f'config-cycle content.user_stylesheets ["{css}"] []')
 
 c.url.default_page = "about:blank"
 c.url.start_pages = ["about:blank"]
