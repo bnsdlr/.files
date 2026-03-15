c5805479019ed02d15bc5fa63d8a0f9bdd95cb94
bnsdlr <benhamster443@gmail.com>
2026-03-15 20:59:56 +0100

    simple tree view script of fs

---
+++ config/scripts/tree.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+FILE_COLOR=""
+DIR_COLOR="\x1b[1;34m"
+NC="\x1b[0m"
+
+print_contents() {
+	local dir=$1
+	local lvl=$2
+
+	local space=$((lvl * 4))
+	local dir_space=$(((lvl - 1) * 4))
+
+	if (( $lvl != 0)); then
+		local bname=$(basename "$dir")
+		printf "%-${dir_space}s${DIR_COLOR}$bname/$NC\n"
+	fi
+
+	for entry in "$dir"/*; do
+		if [[ -d "$entry" ]]; then
+			print_contents "$entry" $((lvl + 1))
+		else
+			local bname=$(basename "$entry")
+			printf "$FILE_COLOR%-${space}s%s$NC\n" "" "$bname"
+		fi
+	done
+}
+
+path="."
+
+if [[ $1 != "" ]]; then
+	path="$1"
+fi
+
+print_contents "$path" 0
