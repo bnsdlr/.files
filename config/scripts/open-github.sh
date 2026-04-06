df1df04a84b4781329e92e3b4d04a5d164628de2
bnsdlr <me@bsdlr.de>
2026-04-06 17:03:41 +0200

    ..

--- config/scripts/open-github.sh
+++ config/scripts/open-github.sh
@@ -10,7 +10,7 @@ url=$(git remote get-url origin)
 # Check if the repository is on GitHub
 if [[ $url == *"github.com"* ]]; then
   open $url
-else if [[ $url == *"codeberg.org"* ]]; then
+elif [[ $url == *"codeberg.org"* ]]; then
   open $url
 else
   echo "This repository is not hosted on GitHub"
